478903e3468a8bc7e6265bec4e28b370f1e332a1
festusisaac <festusisaac848@gmail.com>
2026-09-18 17:28:13 +0100

    test: add setup-env.js to load test environment variables

--- backend/test/setup-env.js
+++ backend/test/setup-env.js
@@ -1,4 +1,4 @@
 const dotenv = require('dotenv');
 const path = require('path');
 
-dotenv.config({ path: path.resolve(__dirname, '../.env.test'), override: true });
+dotenv.config({ path: path.resolve(__dirname, '../.env.test') });
